b5bef33eae11c49c29d328a887126d7efed1bc51
Jue <viny1976@gmail.com>
2024-02-05 10:54:21 +0400

    first commit

---
+++ frontend/src/app/[lang]/blog/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { useState, useEffect, useCallback } from "react";
+import { fetchAPI } from "../utils/fetch-api";
+
+import Loader from "../components/Loader";
+import Blog from "../views/blog-list";
+import PageHeader from "../components/PageHeader";
+
+interface Meta {
+  pagination: {
+    start: number;
+    limit: number;
+    total: number;
+  };
+}
+
+export default function Profile() {
+  const [meta, setMeta] = useState<Meta | undefined>();
+  const [data, setData] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = useCallback(async (start: number, limit: number) => {
+    setIsLoading(true);
+    try {
+      const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+      const path = `/articles`;
+      const urlParamsObject = {
+        sort: { createdAt: "desc" },
+        populate: {
+          cover: { fields: ["url"] },
+          category: { populate: "*" },
+          authorsBio: {
+            populate: "*",
+          },
+        },
+        pagination: {
+          start: start,
+          limit: limit,
+        },
+      };
+      const options = { headers: { Authorization: `Bearer ${token}` } };
+      const responseData = await fetchAPI(path, urlParamsObject, options);
+
+      if (start === 0) {
+        setData(responseData.data);
+      } else {
+        setData((prevData: any[] ) => [...prevData, ...responseData.data]);
+      }
+
+      setMeta(responseData.meta);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  function loadMorePosts(): void {
+    const nextPosts = meta!.pagination.start + meta!.pagination.limit;
+    fetchData(nextPosts, Number(process.env.NEXT_PUBLIC_PAGE_LIMIT));
+  }
+
+  useEffect(() => {
+    fetchData(0, Number(process.env.NEXT_PUBLIC_PAGE_LIMIT));
+  }, [fetchData]);
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <div>
+      <PageHeader heading="Notre Blog" text="Echangez une anecdote entre vous" />
+      <Blog data={data}>
+        {meta!.pagination.start + meta!.pagination.limit <
+          meta!.pagination.total && (
+          <div className="flex justify-center">
+            <button
+              type="button"
+              className="px-6 py-3 text-sm rounded-lg hover:underline dark:bg-gray-900 dark:text-gray-400"
+              onClick={loadMorePosts}
+            >
+              Chargement de plus d'article...
+            </button>
+          </div>
+        )}
+      </Blog>
+    </div>
+  );
+}
